c92816768b5accc884f2fd46daedc1712f53a8ca
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-02-13 01:23:34 +0530

    Time: 144 ms (51.5%), Space: 47 MB (45.5%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -3,13 +3,13 @@ class Solution {
         int n = s.length();
         int maxLength = 0;
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int[] freq = new int[26];
-            for(int j=i; j<n; j++){
+            for (int j = i; j < n; j++) {
                 freq[s.charAt(j) - 'a']++;
 
-                if(checkBalanced(freq)){
-                    maxLength = Math.max(maxLength, j-i+1);
+                if (checkBalanced(freq)) {
+                    maxLength = Math.max(maxLength, j - i + 1);
                 }
             }
         }
@@ -17,15 +17,15 @@ class Solution {
         return maxLength;
     }
 
-    private boolean checkBalanced(int[] freq){
+    private boolean checkBalanced(int[] freq) {
         int common = 0;
 
-        for(int i=0; i<26; i++){
-            if(freq[i] == 0)
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0)
                 continue;
-            if(common == 0)
+            if (common == 0)
                 common = freq[i];
-            else if(common != freq[i])
+            if (common != freq[i])
                 return false;
         }
 
