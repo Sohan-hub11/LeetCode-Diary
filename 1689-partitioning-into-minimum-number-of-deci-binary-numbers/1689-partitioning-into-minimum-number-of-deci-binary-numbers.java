9a61420e524c1c9e41b02456e506cb206ae16911
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-03-02 01:41:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minPartitions(String n) {
+        int max = 0;
+        for(int i=0; i<n.length(); i++){
+            int curr = n.CharAt(i) - '0';
+            max = Math.max(max, curr);
+        }
+
+        return max;
+    }
+}
