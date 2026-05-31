da92776f0f767a4bbc56b5fc7f4f37f8cb3685fb
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-05-31 14:43:44 +0530

    Time: 10 ms (95.61%), Space: 107.8 MB (5.77%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,3 +1,6 @@
+//Sorting Approach T.C -- O(n logn)
+
+/*
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         long m = mass;
@@ -8,6 +11,33 @@ class Solution {
                 m += num;
             } else {
                 return false;
+            }
+        }
+
+        return true;
+    }
+}
+*/
+
+//Counting Array T.C -- O(n)
+
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        long m = mass;
+
+        long[] arr = new long[100001];
+
+        for(int num: asteroids){
+            arr[num]++;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > 0){
+                if(m >= i){
+                    m += (i*arr[i]);
+                } else {
+                    return false;
+                }
             }
         }
 
