07003578c5de17cb41a5f0791c0ac178b314246e
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-06-14 21:19:49 +0530

    Time: 1 ms (99.01%), Space: 42.8 MB (12.82%) - LeetHub

--- 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.java
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.java
@@ -1,5 +1,8 @@
+/* Instead of checking every bit manually, we can shift the number right by one position.
+*/
 class Solution {
     public boolean consecutiveSetBits(int n) {
+        //T.C -- O(1), S.C -- O(1)
         return isPowerOfTwo(n & (n << 1));
     }
     boolean isPowerOfTwo(int n){
