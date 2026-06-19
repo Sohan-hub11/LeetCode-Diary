8f5b80756a0a3dfb86ff3b3dc619d8cb9e49808d
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-06-19 20:17:36 +0530

    Time: 0 ms (100%), Space: 42.9 MB (87.36%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,20 +1,14 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        //T.C -- O(2n+1), S.C -- O(n+1)
-        
+        //T.C -- O(n), S.C -- O(1)
+
         int ans = Integer.MIN_VALUE;
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
-        int sum = 0;
+        int curr = 0;
         for(int num: gain){
-            sum += num;
-            list.add(sum);
+            ans = Math.max(ans, curr);
+            curr += num;
         }
 
-        for(int i=0; i<list.size(); i++){
-            ans = Math.max(ans, list.get(i));
-        }
-
-        return ans;
+        return Math.max(ans, curr);
     }
 }
