16268f6cf2af6665521ce1e6f39ab5f56cc0a6c7
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-06-21 11:47:32 +0530

    Time: 10 ms (80.5%), Space: 82.5 MB (94.63%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,3 +1,6 @@
+/* Brute Force Approach */
+
+/* 
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
         //T.C -- O(n), S.C -- O(n)
@@ -11,6 +14,32 @@ class Solution {
             if(cost <= coins){
                 coins -= cost;
                 ans++;
+            }
+        }
+
+        return ans;
+    }
+}
+*/
+
+/* Using Counting Sort */
+
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        //T.C -- O(n), S.C -- O(n)
+
+        int ans = 0;
+        int[] count = new int[100001];
+
+        for(int cost: costs){
+            count[cost]++;
+        }
+
+        for(int i=1; i<count.length; i++){
+            while(count[i] > 0 && i <= coins){
+                ans++;
+                coins -= i;
+                count[i]--;
             }
         }
 
